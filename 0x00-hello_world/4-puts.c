14e1043e63f0c3ff81fee4106875ae6102297bf4
AFK <nifty_afk@yahoo.com>
2022-09-08 12:47:47 -0700

    put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-   /*
+   /**
 	main - Write 'Programming is like building a multilingual puzzle'
 	Return: Always 0 (success)
    */
@@ -8,8 +8,7 @@
 int main(void)
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 
 }
