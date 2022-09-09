ca97a619ec1e0b12a3a574f2cd77090f99b2efba
AFK <nifty_afk@yahoo.com>
2022-09-09 15:52:23 -0700

    corrections

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,20 +17,21 @@ int main(void)
 	while (n1 <= 57)
 	{
 		n2 = n1 + 1;
-
+		
 		while (n2 <= 57)
 		{
 			n3 = n2 + 1;
-																				while (n3 <= 57)
+			
+			while (n3 <= 57)
 			{
 				putchar(n1);
 				putchar(n2);
 				putchar(n3);
-																					if (n1 != 55 || n2 != 56 || n3 != 57)
-				{
-					putchar(com);
-					putchar(32);
-				}
+			if (n1 != 55 || n2 != 56 || n3 != 57)
+			{
+				putchar(com);
+				putchar(32);
+			}
 			n3 += 1;
 			}
 		n2 += 1;
@@ -38,6 +39,5 @@ int main(void)
 	n1 += 1;
 	}
 	putchar('\n');
-
 	return (0);
 }
